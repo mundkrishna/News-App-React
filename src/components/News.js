4462e41d4a4ee83857f3bca87da5a90f26be6d7a
Krishna Kumari <krishnakumari@Krishnas-MacBook-Air.local>
2023-10-22 23:41:20 +0530

    new app

---
+++ src/components/News.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem';
+import Spinner from './Spinner';
+import PropTypes from "prop-types"
+
+export default class News extends Component {
+    // articles = [
+    //     {
+    //         "source": {
+    //             "id": "bbc-sport",
+    //             "name": "BBC Sport"
+    //         },
+    //         "author": null,
+    //         "title": "Listen: World Cup - Netherlands v Sri Lanka",
+    //         "description": "Listen to BBC Radio 5 Sports Extra commentary as the Netherlands play Sri Lanka in the Men's Cricket World Cup 2023.",
+    //         "url": "http://www.bbc.co.uk/sport/live/cricket/66858381",
+    //         "urlToImage": "https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.23.3/images/bbc-sport-logo.png",
+    //         "publishedAt": "2023-10-21T05:22:13.5639908Z",
+    //         "content": "You need one to watch live TV on any channel or device, and BBC programmes on iPlayer. Its the law.\r\nFind out more"
+    //     },
+    //     {
+    //         "source": {
+    //             "id": "espn-cric-info",
+    //             "name": "ESPN Cric Info"
+    //         },
+    //         "author": null,
+    //         "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+    //         "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+    //         "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+    //         "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+    //         "publishedAt": "2020-04-27T11:41:47Z",
+    //         "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+    //     },
+    //     {
+    //         "source": {
+    //             "id": "espn-cric-info",
+    //             "name": "ESPN Cric Info"
+    //         },
+    //         "author": null,
+    //         "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+    //         "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+    //         "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+    //         "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+    //         "publishedAt": "2020-03-30T15:26:05Z",
+    //         "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+    //     }
+    // ]
+
+    static defaultProps = {
+        country: "in",
+        pageSize: 8,
+        category: "general"
+    }
+
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string,
+    }
+
+    constructor() {
+        super();
+        // console.log("Hello I am a constructor from News component");
+        this.state = {
+            articles: [],
+            loading: false,
+            page: 1,
+        }
+    }
+
+    async componentDidMount() {
+        // console.log("cdm");
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f341d831831c463e8ebbbefe7f01d87c&page=1&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true });
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults,
+            loading: false
+        });
+    }
+
+    handlePreviousClick = async () => {
+        console.log("Previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f341d831831c463e8ebbbefe7f01d87c&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true });
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles,
+            loading: false
+        });
+    }
+
+    handleNextClick = async () => {
+        console.log("Next");
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f341d831831c463e8ebbbefe7f01d87c&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true });
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+            page: this.state.page + 1,
+            articles: parsedData.articles,
+            loading: false
+        });
+    }
+
+    render() {
+        // console.log("render");
+        return (
+            <div className="container my-3">
+                <h1 className="text-center" style={{ margin: "35px 0px" }}>NewsApp - Top Headlines</h1>
+                {this.state.loading && <Spinner />}
+                <div className="row">
+                    {!this.state.loading && this.state.articles.map((element) => {
+                        return (
+                            <div className="col-md-4" key={element.url}>
+                                <NewsItem
+                                    title={element && element.title ? element.title.slice(0, 43) : ""}
+                                    description={element && element.description ? element.description.slice(0, 70) : ""}
+                                    imageUrl={element.urlToImage}
+                                    newsUrl={element.url}
+                                />
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&larr; Previous</button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
+            </div>
+        )
+    }
+}
