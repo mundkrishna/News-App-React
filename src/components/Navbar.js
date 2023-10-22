a3efb62863d6f2b98062d0396c8e0b129ca0ed76
Krishna Kumari <krishnakumari@Krishnas-MacBook-Air.local>
2023-10-23 00:35:17 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
     render() {
@@ -6,38 +7,38 @@ export default class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/" style={{ color: "white" }}>NewsApp</a>
+                        <Link className="navbar-brand" to="/" style={{ color: "white" }}>NewsApp</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/" style={{ color: "white" }}>Home</a>
+                                    <Link className="nav-link" aria-current="page" to="/" style={{ color: "white" }}>Home</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about" style={{ color: "white" }}>About</a>
+                                    <Link className="nav-link" to="/about" style={{ color: "white" }}>About</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about" style={{ color: "white" }}>Business</a>
+                                    <Link className="nav-link" to="/business" style={{ color: "white" }}>Business</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about" style={{ color: "white" }}>Entertainment</a>
+                                    <Link className="nav-link" to="/entertainment" style={{ color: "white" }}>Entertainment</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about" style={{ color: "white" }}>General</a>
+                                    <Link className="nav-link" to="/general" style={{ color: "white" }}>General</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about" style={{ color: "white" }}>Health</a>
+                                    <Link className="nav-link" to="/health" style={{ color: "white" }}>Health</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about" style={{ color: "white" }}>Science</a>
+                                    <Link className="nav-link" to="/science" style={{ color: "white" }}>Science</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about" style={{ color: "white" }}>Sports</a>
+                                    <Link className="nav-link" to="/sports" style={{ color: "white" }}>Sports</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about" style={{ color: "white" }}>Technology</a>
+                                    <Link className="nav-link" to="/technology" style={{ color: "white" }}>Technology</Link>
                                 </li>
                             </ul>
                         </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,8 +5,12 @@ export default class NewsItem extends Component {
         let { title, description, imageUrl, newsUrl } = this.props;
         return (
             <div className="my-3">
-                <div className="card" style={{ width: "18rem" }}>
-                    <img src={!imageUrl ? "https://www.livemint.com/lm-img/img/2023/10/21/1600x900/deded_1697896241977_1697896251852.jpg" : imageUrl} className="card-img-top" alt="..." />
+                <div className="card">
+                    <img
+                        src={!imageUrl ? "https://www.livemint.com/lm-img/img/2023/10/21/1600x900/deded_1697896241977_1697896251852.jpg" : imageUrl}
+                        className="card-img-top"
+                        alt="..."
+                        style={{ width: "415px", height: "260px" }} />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
